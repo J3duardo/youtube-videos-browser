418ce3db1a754298beb969901920ad27da6e87f5
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-15 17:01:51 -0400

    Refactorizado el componente VideoList a componente de clase

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import VideoItem from "./VideoItem";
 
-const VideoList = (props) => {
-  const videosList = props.videos.map((video) => {
-    return <VideoItem key={video.id.videoId} video={video}/>
-  })
+class VideoList extends Component {
+  videosList = () => {
+    return this.props.videos.map((video) => {
+      return <VideoItem key={video.id.videoId} video={video}/>
+    })
+  }
 
-  return (
-    <div className="ui relaxed divided list">
-      {videosList}
-    </div>
-  );
+  render() {
+    return (
+      <div className="ui relaxed divided list">
+        {this.videosList()}
+      </div>
+    );
+  }
 }
 
 export default VideoList;
