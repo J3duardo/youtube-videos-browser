828ec23add952e0729becdc67b409574d034cfa3
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-15 12:02:21 -0400

    Implementado el componente VideoItem y el className de semantic en el componente VideoList

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoItem from "./VideoItem";
 
 const VideoList = (props) => {
+  const videosList = props.videos.map((video) => {
+    return <VideoItem key={video.id.videoId} video={video}/>
+  })
+
   return (
-    <div>
-      Video List
+    <div className="ui relaxed divided list">
+      {videosList}
     </div>
   );
 }
