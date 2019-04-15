5b0f4e12f1b2a6745501396938ddd6f46d5cebd2
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-15 11:08:55 -0400

    Implementado el componente VideoList en App

--- src/componentes/app.js
+++ src/componentes/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from "./SearchBar";
 import youtube from "./apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends Component {
   state = {
@@ -23,6 +24,7 @@ class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onSearchSubmit}/>
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
