9deb1bfaff665d48888b5696cc63b6a73395b5c4
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-15 11:56:47 -0400

    Agregados los estilos de cada item en la lista de videos

--- src/componentes/VideoItem.js
+++ src/componentes/VideoItem.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import "./VideoItem.css";
 
-const VideoItem = () => {
+const VideoItem = (props) => {
+  const snippet = props.video.snippet;
   return (
-    <div>
-      Video Item
+    <div className="item" id="video-item">
+      <img
+        className="ui image"
+        src={snippet.thumbnails.high.url}
+        alt={snippet.description}
+      />
+      <div className="content">
+        <div className="header">
+          {props.video.snippet.title}
+        </div>
+        <p className="video-description">{snippet.description}</p>
+      </div>
     </div>
   );
 }
