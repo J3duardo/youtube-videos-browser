11632f0c65d0884ffd3ff45b095ba529112627ef
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-15 09:01:43 -0400

    Creado el método onChangeHandler en el componente SearchBar
    El método se encarga de tomar el value del input para actualizar el searchTerm en el state, el cual es tomado por el input como su value

--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  state = {
+    searchTerm: ""
+  }
+
+  onChangeHandler = (e) => {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="ui segment search-bar">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text"/>
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.onChangeHandler}
+            />
           </div>
         </form>     
       </div>
