5689913732cb8422e4b9ea133ec3326ab8a78ea4
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-15 10:08:13 -0400

    Creado el método onSubmitHandler en SearchBar.
    El método se encarga de comunicar el término de búsqueda al componente App para que se ejecute la solicitud a la api con dicho término como parámetro de búsqueda.

--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -11,10 +11,15 @@ class SearchBar extends Component {
     })
   }
 
+  onSubmitHandler = (e) => {
+    e.preventDefault()
+    this.props.onFormSubmit(this.state.searchTerm);
+  }
+
   render() {
     return (
       <div className="ui segment search-bar">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onSubmitHandler}>
           <div className="field">
             <label>Video Search</label>
             <input
